8279c6e07c32b8bf0fd817c0af772e76d9bc0a23
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2024-02-28 21:55:00 +1100

    add:dbcontext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using ficaqui_api.Models;
 
 namespace ficaqui_api.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Search> Searches { get; set; }
     }
 }
